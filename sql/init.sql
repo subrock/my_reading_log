a75dc9edc4189f084c9c301cd774722eed436bc2
subrock <subrock@gmail.com>
2015-06-05 12:10:08 -0700

    updated init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,8 +1,8 @@
 
 
-DROP DATABASE if EXISTS STAT16;
-CREATE DATABASE IF NOT EXISTS STAT16;
-USE STAT16;
+DROP DATABASE if EXISTS MY_READING_LOG;
+CREATE DATABASE IF NOT EXISTS MY_READING_LOG;
+USE MY_READING_LOG;
 
 DROP TABLE IF EXISTS `AdminTable`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -26,17 +26,36 @@ UNLOCK TABLES;
 
 
 
-DROP TABLE IF EXISTS `TeamTable`;
+DROP TABLE IF EXISTS `ReaderTable`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `TeamTable` (
-  `team_id` int(6) unsigned NOT NULL AUTO_INCREMENT,
-  `team_name` varchar(30) NOT NULL,
-  `team_contact` varchar(30) NOT NULL,
-  `team_password` varchar(30) NOT NULL,
-  `team_email` varchar(50) DEFAULT NULL,
+CREATE TABLE `ReaderTable` (
+  `reader_id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `reader_name` varchar(30) NOT NULL,
+  `reader_password` varchar(30) NOT NULL,
+  `reader_email` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`team_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `EntryTable`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `EntryTable` (
+  `entry_id` mediumint(5) NOT NULL AUTO_INCREMENT,
+  `entry_date` varchar(250) DEFAULT NULL,
+  `entry_title` varchar(250) DEFAULT NULL,
+  `entry_author` varchar(250) DEFAULT NULL,
+  `entry_level` varchar(25) DEFAULT NULL,
+  `entry_genre` varchar(25) DEFAULT NULL,
+  `entry_complete` varchar(25) DEFAULT NULL,
+  `entry_start` smallint(8) DEFAULT NULL,
+  `entry_end` smallint(8) DEFAULT NULL,
+  `entry_minutes` smallint(8) DEFAULT NULL,
+  `entry_real_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`entry_id`),
+  UNIQUE KEY `entry_id` (`entry_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=255 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
