7023a6a3b32f8abc4b89ca09518b4b8e5d495bc4
subrock <subrock@gmail.com>
2015-06-27 12:50:52 -0700

    Updated about font color.

--- web-client/src/about/index.php
+++ web-client/src/about/index.php
@@ -1,1 +1,19 @@
-My Reading Log for Android
+<html>
+<head>
+</head>
+<body style="margin:10;padding:10" alink=silver text=white vlink=navy link=navy onload="load()" bgcolor=#303030>
+<Br><Br>
+<center><h1>My Reading Log for Android</h1>
+
+<table width=500>
+<tr>
+<Td><font face=Verdana>
+My Reading Log is an application that allows you to keep track of a students reading progress. It also provides useful metrics allowing educators to quickly evaluate a students progress. 
+</td>
+</tr>
+</table>
+
+</center>
+
+</body>
+</html>
